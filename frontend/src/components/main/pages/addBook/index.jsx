e5dfb4cc3bc750118cb57bf5c89bd41e1c11c69e
niv3D <nived807@gmail.com>
2021-05-26 12:36:57 +0530

    :anger:

--- frontend/src/components/main/pages/addBook/index.jsx
+++ frontend/src/components/main/pages/addBook/index.jsx
@@ -1,8 +1,35 @@
-import react from "react";
+import react, { useState } from "react";
 import "./style.css";
 
 function Addbook() {
-  
+  const [ibn, setIbn] = useState("");
+  const [ibnValid, setIbnValid] = useState(true);
+  const [title, setTitle] = useState("");
+  const [titleValid, setTitleValid] = useState(true);
+  const [author, setAuthor] = useState("");
+  const [authorValid, setAuthorValid] = useState(true);
+  const [language, setLanguage] = useState("");
+  const [languageValid, setLanguageValid] = useState(true);
+  const [year, setYear] = useState("");
+  const [yearValid, setYearValid] = useState(true);
+  const [genre, setGenre] = useState("");
+  const [genreValid, setGenreValid] = useState(true);
+  const [descritpion, setDescription] = useState("");
+  const [descriptionValid, setDescriptionValid] = useState(true);
+  const [link, setLink] = useState("");
+  const [linkValid, setLinkValid] = useState(true);
+
+  const handleBook = async (e) => {
+    e.preventDefault();
+    setIbnValid(true);
+    setTitleValid(true);
+    setAuthorValid(true);
+    setLanguageValid(true);
+    setYearValid(true);
+    setGenreValid(true);
+    setDescriptionValid(true);
+    setLinkValid(true);
+  };
   return (
     <>
       <div className="Addbook">
@@ -14,7 +41,7 @@ function Addbook() {
           <form className="addbook-form">
             <h2>Add Book</h2>
             <p>
-              <label>IBN Number : </label>
+              <label>ISBN Number : </label>
               <input
                 className="addbook-field"
                 style={{ width: "30%" }}
--- frontend/src/components/main/pages/landing/index.jsx
+++ frontend/src/components/main/pages/landing/index.jsx
@@ -7,7 +7,7 @@ function LandingPage() {
   useEffect(() => {
     async function getData() {
       try {
-        const res = await api.post("/view");
+        const res = await api.post("/view", { genre: "all" });
         if (res.data) {
           setData(res.data);
         }
@@ -19,29 +19,33 @@ function LandingPage() {
   }, []);
   return (
     <>
-      { data ? data.status ? 
-        <div className="book-tile">
-          <img src="/dummy.jpg" />
-          <div className="book-details">
-            <div className="book-overview">
-              <p className="book-name">
-                <h2>Name of the book</h2>
-                <b>Author name</b>
-              </p>
-              <p className="book-rating">
-                <h2>5.4</h2>
-                <b>(6,592)</b>
-              </p>
-            </div>
-            <p className="book-description">
-              <b>Description :</b> <br />
-              This book is a training material that covers all aspects of
-              Forensic Science with 30 chapters, explaining different types of
-              crime scenes, the probable types of evidence expec...
-            </p>
-          </div>
-        </div>
-      : "No books" : "please wait"}
+      {data
+        ? data.status
+          ? data.data.map((book, index) => (
+              <a href={"/book/" + book.ibn}>
+                <div key={index} className="book-tile">
+                  <img src={book.cover_img} />
+                  <div className="book-details">
+                    <div className="book-overview">
+                      <p className="book-name">
+                        <h2>{book.title}</h2>
+                        <b>{book.author}</b>
+                      </p>
+                      <p className="book-rating">
+                        <h2>{book.avg_rating}</h2>
+                        <b>({book.reviews.length})</b>
+                      </p>
+                    </div>
+                    <p className="book-description">
+                      <b>Description :</b> <br />
+                      {book.description}
+                    </p>
+                  </div>
+                </div>
+              </a>
+            ))
+          : "No books"
+        : "please wait"}
     </>
   );
 }
