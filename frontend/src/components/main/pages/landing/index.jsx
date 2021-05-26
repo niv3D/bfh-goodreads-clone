85a754b4d56f53c89b329834043d7ee3e2d8bb42
niv3D <nived807@gmail.com>
2021-05-26 18:06:03 +0530

    final

--- frontend/src/components/main/pages/landing/index.jsx
+++ frontend/src/components/main/pages/landing/index.jsx
@@ -25,23 +25,25 @@ function LandingPage() {
               <div key={index} className="book-tile">
                 <a href={"/book/" + book.ibn}>
                   <img src={book.cover_img} />
-                </a>
-                <div className="book-details">
-                  <div className="book-overview">
-                    <p className="book-name">
-                      <h2>{book.title}</h2>
-                      <b>{book.author}</b>
-                    </p>
-                    <p className="book-rating">
-                      <h2>{book.avg_rating}</h2>
-                      <b>({book.reviews.length})</b>
+
+                  <div className="book-details">
+                    <div className="book-overview">
+                      <p className="book-name">
+                        <h2>{book.title}</h2>
+                        <b>{book.author}</b>
+                      </p>
+
+                      <p className="book-rating">
+                        <h2>{book.avg_rating}</h2>
+                        <b>({book.reviews.length})</b>
+                      </p>
+                    </div>
+                    <p className="book-description">
+                      <b>Description :</b> <br />
+                      {book.description}
                     </p>
                   </div>
-                  <p className="book-description">
-                    <b>Description :</b> <br />
-                    {book.description}
-                  </p>
-                </div>
+                </a>
               </div>
             ))
           : "No books"
